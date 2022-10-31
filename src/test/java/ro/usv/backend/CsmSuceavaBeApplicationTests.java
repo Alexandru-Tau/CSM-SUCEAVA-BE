aa3d7476b8c7744a14ca887338979b5ca55a6e7b
Alex <alextauteanu99@gmail.com>
2022-10-31 19:51:47 +0200

    Initial commit

---
+++ src/test/java/ro/usv/backend/CsmSuceavaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.usv.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsmSuceavaBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
